ae64723ff7ddc1e6970fce5285f3ffd0d6894242
Emmanuel <kcemmy03@gmail.com>
2025-04-08 16:29:13 +0100

    Modification to stack data structure and methods

--- types/stack.go
+++ types/stack.go
@@ -2,9 +2,8 @@ package types
 
 import (
 	"errors"
-	"fmt"
-	"math/big"
-	"os"
+
+	"github.com/holiman/uint256"
 )
 
 const MAX_STACK_SIZE = 1024
@@ -16,7 +15,7 @@ var (
 
 // Stack is a list of 32 bytes elements
 type Stack struct {
-	elem [][]byte // A list of 32 bytes elements
+	elem []*uint256.Int // A list of 32 bytes elements
 }
 
 // var stack = Stack{
@@ -25,63 +24,80 @@ type Stack struct {
 
 func NewStack() *Stack {
 	return &Stack{
-		elem: make([][]byte, MAX_STACK_SIZE),
+		elem: make([]*uint256.Int, 0, MAX_STACK_SIZE), // Initialize the stack with a capacity of MAX_STACK_SIZE
 	}
 }
 
-// ToBigInt returns the n-th element of the stack as a big.Int
-func (stack *Stack) ToBigInt(n int) *big.Int {
-	if len(stack.elem) == 0 {
-		fmt.Fprintln(os.Stderr, ErrStackUnderflow.Error())
-		return nil
-	}
-	return new(big.Int).SetBytes(stack.elem[len(stack.elem)-n])
-}
+// // ToBigInt returns the n-th element of the stack as a big.Int
+// func (stack *Stack) ToBigInt(n int) *big.Int {
+// 	if len(stack.elem) == 0 {
+// 		fmt.Fprintln(os.Stderr, ErrStackUnderflow.Error())
+// 		return nil
+// 	}
+// 	return new(big.Int).SetBytes(stack.elem[len(stack.elem)-n])
+// }
 
 // Push adds a new element to the stack
-func (stack *Stack) Push(value []byte) {
+func (stack *Stack) Push(value *uint256.Int) error {
 	if len(stack.elem) >= MAX_STACK_SIZE {
-		fmt.Fprintln(os.Stderr, ErrStackOverflow.Error())
-		return
+		return ErrStackOverflow
 	}
 	stack.elem = append(stack.elem, value)
+	return nil
 }
 
 // Pop removes the last element from the stack
-func (stack *Stack) Pop() []byte {
+func (stack *Stack) Pop() (*uint256.Int, error) {
 	if len(stack.elem) == 0 {
-		fmt.Fprintln(os.Stderr, ErrStackUnderflow.Error())
-		return []byte{}
+		return nil, ErrStackUnderflow
 	}
 	value := stack.elem[len(stack.elem)-1]
 	stack.elem = stack.elem[:len(stack.elem)-1]
-	return value
+	return value, nil
 }
 
 // Peek returns the last element from the stack without removing it
-func (stack *Stack) Peek() []byte {
+func (stack *Stack) Peek() (*uint256.Int, error) {
 	if len(stack.elem) == 0 {
-		fmt.Fprintln(os.Stderr, ErrStackUnderflow.Error())
-		return []byte{}
+		return nil, ErrStackUnderflow
 	}
-	return stack.elem[len(stack.elem)-1]
+	return stack.elem[len(stack.elem)-1], nil
 
 }
 
 // Swap swaps the n-th element from the top of the stack with the top element
-func (stack *Stack) Swap(n int) {
+func (stack *Stack) Swap(n int) error {
 	if len(stack.elem) < 2 {
-		fmt.Fprintln(os.Stderr, ErrStackUnderflow.Error())
-		return
+		return ErrStackUnderflow
 	}
 	stack.elem[len(stack.elem)-1], stack.elem[len(stack.elem)-1-n] = stack.elem[len(stack.elem)-1-n], stack.elem[len(stack.elem)-1]
+	return nil
 }
 
 // Dup duplicates the n-th element from the top of the stack
-func (stack *Stack) Dup(n int) {
+func (stack *Stack) Dup(n int) error {
 	if len(stack.elem) < 1 {
-		fmt.Fprintln(os.Stderr, ErrStackUnderflow.Error())
-		return
+		return ErrStackUnderflow
 	}
 	stack.elem = append(stack.elem, stack.elem[len(stack.elem)-n])
+	return nil
+}
+
+// ToString method returns the string representation of the stack
+func (stack *Stack) ToString() string {
+	var s string
+
+	if len(stack.elem) == 0 {
+		s = "[]"
+		return s
+	}
+	s = "["
+	for i := len(stack.elem) - 1; i >= 0; i-- {
+		s += stack.elem[i].Hex() // Use Hex() to get the hex representation of the element
+		if i != 0 {
+			s += ", "
+		}
+	}
+	s += "]"
+	return s
 }
